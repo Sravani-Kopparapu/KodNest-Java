13cb38382f91e9c6afb6c97a7c9d510b8815a436
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-08-04 12:29:24 +0530

    Checking if number is zero

--- track/Month2-Java/JavaPractice/Number.java
+++ track/Month2-Java/JavaPractice/Number.java
@@ -2,7 +2,7 @@
 public class Number {
 
     public static void main(String[] args) {
-        int number = 10;
+        int number = 0;
         if (number > 0) {
             System.out.println("Positive");
         } else if (number == 0) {
