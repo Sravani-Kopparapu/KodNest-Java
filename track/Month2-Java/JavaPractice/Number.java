9cd873bfa43b58760f50866a2c00415dc1cc0efa
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-08-04 12:27:17 +0530

    checking number

---
+++ track/Month2-Java/JavaPractice/Number.java
@@ -0,0 +1,14 @@
+
+public class Number {
+
+    public static void main(String[] args) {
+        int number = -1;
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
