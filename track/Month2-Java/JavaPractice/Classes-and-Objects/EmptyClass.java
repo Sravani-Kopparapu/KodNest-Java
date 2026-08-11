bfb80eb9547668c84df4e7a6142c7cc46819178d
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-08-11 11:14:11 +0530

    Empty class compiled succesfully

---
+++ track/Month2-Java/JavaPractice/Classes-and-Objects/EmptyClass.java
@@ -0,0 +1,12 @@
+
+class Empty {
+
+}
+
+class EmptyClass {
+
+    public static void main(String[] args) {
+        Empty e1 = new Empty();
+        System.out.println("Empty class compiled successfully");
+    }
+}
