4638c8caf2c10188a43c0ef56cf2110a5bada477
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-08-12 15:31:06 +0530

    Non primitive data types have ambiguity when it comes to overloading

--- track/Month2-Java/JavaPractice/Classes-and-Objects/AmbiguityMO.java
+++ track/Month2-Java/JavaPractice/Classes-and-Objects/AmbiguityMO.java
@@ -1,11 +1,11 @@
 
 class A {
 
-    void m1(short a, short b) {
-        System.out.println("short method" + a + " " + b);
+    void m1(String a, String b) {
+        System.out.println("String method" + a + " " + b);
     }
 
-    void m1(int a, int b) {
+    void m1(A a, A b) {
         System.out.println("Int method" + a + " " + b);
     }
 }
@@ -14,6 +14,6 @@ class AmbiguityMO {
 
     public static void main(String[] args) {
         A a = new A();
-        a.m1(1, 1);
+        a.m1(null, null); // both are no primitive type values and both the methods are accepting non primitive as parameters, so here rises the ambiguity.
     }
 }
