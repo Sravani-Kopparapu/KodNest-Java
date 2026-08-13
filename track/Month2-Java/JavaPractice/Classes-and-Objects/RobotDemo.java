a6619ad24709274404d6e1b4e7aa6f0d4f8aace1
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-08-13 23:00:33 +0530

    RobotDemo

---
+++ track/Month2-Java/JavaPractice/Classes-and-Objects/RobotDemo.java
@@ -0,0 +1,15 @@
+
+class Robot {
+
+    void speak() {
+        System.out.println("Beep beep! Java is my superpower.");
+    }
+}
+
+class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot r1 = new Robot();
+        r1.speak();
+    }
+}
