c52e8be63883a0c604513bbf9285f9bd41fef098
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-08-18 11:00:10 +0530

    Array sum

---
+++ track/Month2-Java/JavaPractice/Arrays/Pgm4.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+class Pgm4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        int sum = 0;
+        for (int i = 0; i <= a.length - 1; i++) {
+            sum += a[i];
+        }
+        System.out.println("Total: " + sum);
+    }
+}
