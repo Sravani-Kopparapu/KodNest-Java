76983125a054d847612d909f74e5a1b738a4e25b
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-09-07 17:25:42 +0530

    Using equals() method

---
+++ track/Month2-Java/JavaPractice/Strings/Pgm3.java
@@ -0,0 +1,13 @@
+
+class Pgm3 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        if (s1.equals(s2)) {
+            System.out.println("Strings are same");
+        } else {
+            System.out.println("Strings are not same");
+        }
+    }
+}
