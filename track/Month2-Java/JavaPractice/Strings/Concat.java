1d532036bd725daa69ce43fb53a682d2e2e05d0a
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-09-20 20:23:30 +0530

    Concatination

---
+++ track/Month2-Java/JavaPractice/Strings/Concat.java
@@ -0,0 +1,15 @@
+
+class Concat {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Tech";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+
+        String t1 = new String("Arshit");
+        String t2 = new String("Ramu");
+        String res = t1.concat(t2);
+        System.out.println(res);
+    }
+}
